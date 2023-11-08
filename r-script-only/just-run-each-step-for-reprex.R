95add93f6457526e50506f1739683cf589a1fc2a
njtierney <nicholas.tierney@gmail.com>
2023-11-08 16:15:35 +1100

    rename the files for clarity

--- r-script-only/just-run-each-step-for-reprex.R
+++ r-script-only/just-run-each-step-for-reprex.R
@@ -1,3 +1,3 @@
-source("r-script-only/just-define-model.R")
-source("r-script-only/just-fit-mcmc.R")
-source("r-script-only/just-plot-output.R")
+source("r-script-only/01-just-define-model.R")
+source("r-script-only/02-just-fit-mcmc.R")
+source("r-script-only/03-just-plot-output.R")
